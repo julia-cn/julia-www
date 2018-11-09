c8ef3f0ee5a0e995d5d0ca57ed0de4a5989a399d
lisniuse <17560235@qq.com>
2018-11-10 02:08:46 +0800

    update.

--- libs/js/cn.js
+++ libs/js/cn.js
@@ -8,14 +8,14 @@ $(function () {
                     '<table>' + 
                         '<thead>' +
                             '<tr>' +
-                                '<th align="center">支付宝</th>' +
-                                '<th align="center">微信</th>' +
+                                '<th style="text-align: center;border: 1px solid #404c58;background: #fff;">支付宝</th>' +
+                                '<th style="text-align: center;border: 1px solid #404c58;background: #fff;">微信</th>' +
                             '</tr>' +
                         '</thead>' +
                         '<tbody>' +
                             '<tr>' +
-                                '<td align="center"><a target="_blank" rel="noopener noreferrer" href="/libs/images/wechat-qr.jpg"><img src="/libs/images/wechat-qr.jpg" width="150" style="max-width:100%;"></a></td>' + 
-                                '<td align="center"><a target="_blank" rel="noopener noreferrer" href="/libs/images/alipay-qr.jpg"><img src="/libs/images/alipay-qr.jpg" width="150" style="max-width:100%;"></a></td>' +
+                                '<td style="text-align: center;border: 1px solid #404c58;"><a target="_blank" rel="noopener noreferrer" href="/libs/images/wechat-qr.jpg"><img src="/libs/images/wechat-qr.jpg" width="150" style="max-width:100%;"></a></td>' + 
+                                '<td style="text-align: center;border: 1px solid #404c58;"><a target="_blank" rel="noopener noreferrer" href="/libs/images/alipay-qr.jpg"><img src="/libs/images/alipay-qr.jpg" width="150" style="max-width:100%;"></a></td>' +
                             '</tr>' + 
                         '</tbody>' +
                     '</table>'
@@ -24,5 +24,6 @@ $(function () {
     var maskEl = $(maskHtml);
     var popEl = $(popHtml);
     maskEl.append(popEl);
+    popEl.addClass("show");
     $("body").append(maskEl);
 });
