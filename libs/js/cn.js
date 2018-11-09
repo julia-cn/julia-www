05f92407d009a6446560032583ba2c2733d3e844
lisniuse <17560235@qq.com>
2018-11-10 01:48:53 +0800

    update.

--- libs/js/cn.js
+++ libs/js/cn.js
@@ -1,3 +1,22 @@
 $(function () {
-    console.log(1);
+    var pop = '<div class="pop">' +
+                '<div class="pop-body">' +
+                    '<div class="text"></div>' + 
+                    '<table>' + 
+                        '<thead>' +
+                            '<tr>' +
+                                '<th align="center">支付宝</th>' +
+                                '<th align="center">微信</th>' +
+                            '</tr>' +
+                        '</thead>' +
+                        '<tbody>' +
+                            '<tr>' +
+                                '<td align="center"><a target="_blank" rel="noopener noreferrer" href="/libs/images/wechat-qr.jpg"><img src="/libs/images/wechat-qr.jpg" width="150" style="max-width:100%;"></a></td>' + 
+                                '<td align="center"><a target="_blank" rel="noopener noreferrer" href="/libs/images/alipay-qr.jpg"><img src="/libs/images/alipay-qr.jpg" width="150" style="max-width:100%;"></a></td>' +
+                            '</tr>' + 
+                        '</tbody>' +
+                    '</table>'
+                '</div>' +
+                '</div>';
+    console.log(pop);
 });
